6a4b1e30803f28d9830eb7b59b4b6463366753a1
amandavmonaco <avmonaco@smu.edu>
2023-07-05 14:27:30 -0700

    reading the property name from file

--- InvestorData/InvestorData.py
+++ InvestorData/InvestorData.py
@@ -10,11 +10,9 @@ from ListOfInvestors import *
 # print(os.listdir(path))
 
 
-
 def InvestorData() :
     try: 
-        excelFile = pd.read_excel("InvestorPropertisPyFile.xlsx")
-        # print(excelFile)
+        excelFile = pd.read_excel("InvestorPropertiesPyFile.xlsx")
     except FileNotFoundError:
         print('Financial distribution file not found')
 
@@ -25,55 +23,42 @@ def InvestorData() :
         filtered_values = [value for value in column_values if not pd.isnull(value)]
         investorKeyDictionary[column] = filtered_values
 
-    #Checking that the dictionary is correct
-    for keys, values in investorKeyDictionary.items():
-        print(keys, ": ", values)
-    
-    propertyKeyDictionary = {}
-
-    # for key,values in investorKeyDictionary.items():
-    #     for i in range(0, len(values)-1):
-    #         newKey = list(investorKeyDictionary.keys()[i])
-    #         print(newKey)
-    #         propertyKeyDictionary[investorKeyDictionary[property]] = [key]
-
-    print("\nPROPERTY KEY DICTIONARY:\n")
-    for keys, values in propertyKeyDictionary.items():
-        print(keys, ": ", values) 
-
-    for i in range(0, len(InvestorNames)-1):   
-        print(InvestorNames[i])
-
-
-    for i in range(0, len(InvestorNames)-1):
-       investorName = InvestorNames[i]
-       allKeys = list(investorKeyDictionary.keys())
-       print("ALL KEYS:\n")
-       print(allKeys)
-       for j in range(0, len(allKeys)-1):
-            print (allKeys[j])
-            listOfOneInvestorsProperties = investorKeyDictionary[allKeys[j]]
-            print(listOfOneInvestorsProperties)
-            # for k in range(0, len())
-            #     propertyKeyDictionary[]
-
-
-    investorList = []
+    properties = set()
     root_directory = 'Investor Financial Data'
     for investor in investorKeyDictionary.keys():
-        for properties in investorKeyDictionary[investor]:
-            dir_name = str(root_directory) + '/' + str(investor) + '/' + str(properties)
+        for property in investorKeyDictionary[investor]:
+            properties.add(property)
+            dir_name = str(root_directory) + '/' + str(investor) + '/' + str(property)
             try:
                 os.makedirs(dir_name)
                 print('Directory ' + dir_name + ' created')
             except FileExistsError:
                 print('Directory ' + dir_name + ' already exists') 
-   
     
-       
-    # word_to_find = "InvestorData"
-    # InvestorNamesRow = excelFile.contains(word_to_find, na=False)
-    # print(InvestorNamesRow)
+    # for i in properties:
+    #     print(i)
+    sourcePath = '/Users/amandamonaco/Documents/Folder Automation/12Month'
+    
+    
+    for fileName in os.scandir(sourcePath):
+        if fileName.is_file():
+            print(fileName)
+            f = str(fileName)
+            endingIndex = f.find('12')
+            # singleQuote = f.find('\'')
+            # doubleQuote = f.find('\"')
+            startingIndex = 11 
+      
+            print(startingIndex, " " ,endingIndex)
+            print(f[startingIndex:endingIndex])
+            for p in properties: 
+                if str(p) == fileName:
+                    print('P: ' + p)
+                
+    
+
+    
+    # print(os.listdir(sourcePath))
 
 
 InvestorData()
