590d98baaac5614a0b4bdbed21553eb9ad78ccb4
amandavmonaco <avmonaco@smu.edu>
2023-08-01 11:52:10 -0700

    One folder populating

--- InvestorData/FolderAutomation.py
+++ InvestorData/FolderAutomation.py
@@ -1,4 +1,72 @@
-import os
+import os 
 import shutil
+import pandas as pd
 
-# def createProperty():
+financialDocumentsPath = '/Users/amandamonaco/Documents/Folder Automation/12Month'
+propertyKeyDictionary = {}
+
+def CreateInvestorPropertyDictionary():
+    try:
+        excelFile = pd.read_excel('InvestorPropertiesPyFile.xlsx')
+    except FileNotFoundError:
+        print('Investor Properties File was not found')
+
+    for column in excelFile:
+        column_values = excelFile[column].tolist()
+        filtered_values = [value for value in column_values if not pd.isnull(value)]
+        propertyKeyDictionary[column] = filtered_values
+
+def TestFunctionToPrintDictionary():
+    for propertyKey in propertyKeyDictionary.keys():
+        print('KEY: ' + propertyKey)
+        for investors in propertyKeyDictionary[propertyKey]:
+            print(' INVESTOR: ' + investors)
+
+
+# Property Key Dictionary is correctly populating and the excel file has been restructured
+
+def CreatePropertyFoldersWithFinancialStatements():
+    root_dir = 'Property Folders'
+    for fileName in os.scandir(financialDocumentsPath):
+        if fileName.is_file():
+            f = str(fileName)
+            endingIndex = f.find('12') - 1
+            findDash = f.find('-')
+            if(findDash != -1):
+                endingIndex = findDash
+            startingIndex = 11
+
+            trimmedFileName = f[startingIndex:endingIndex]
+            trimmedFileName[:-1]
+            
+            propFolderName = str(root_dir) + '/' + str(trimmedFileName)
+            try:
+                os.makedirs(propFolderName)
+                print(propFolderName+ ' made')
+            except FileExistsError:
+                print('Folder', trimmedFileName, 'already exists')
+                
+            shutil.copy(fileName, propFolderName)
+
+
+def CreateInvestorFoldersWithPropertyStatements():
+    root_dir = 'Investor Folders'
+
+    for folder in os.scandir('Property Folders'):
+        investorNames = propertyKeyDictionary.get(folder.name)
+        # print(' PROP NAME: ' + folder.name + ' INVESTOR NAMES: ' + str(propName))
+        if investorNames != None:  
+            for i in investorNames:
+                try: 
+                    path = os.makedirs(str(root_dir) + '/' + i)
+                except FileExistsError:
+                    print(i + ' folder already created')
+                shutil.move('Property Folders/' + folder.name, 'Investor Folders/' + i)
+                
+                
+
+
+CreateInvestorPropertyDictionary()
+# TestFunctionToPrintDictionary()
+CreatePropertyFoldersWithFinancialStatements()
+CreateInvestorFoldersWithPropertyStatements()
